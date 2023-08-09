b5d581b5af10e9e273db3bb03208c3f314924320
Omkar Andhare <andhare1999@gmail.com>
2023-08-09 13:56:18 +0530

    removed unneccesory packages

--- Internet_Banking_Application/src/main/java/com/iba/main/bankdetails/service/BankDetailsServiceImpl.java
+++ Internet_Banking_Application/src/main/java/com/iba/main/bankdetails/service/BankDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.iba.main.bankdetails.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
 import com.iba.main.bankdetails.repo.BankDetailsRepository;
