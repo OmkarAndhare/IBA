888c20b936dd92309660d158b8ef925c9c6de523
Omkar Andhare <andhare1999@gmail.com>
2023-08-18 21:12:01 +0530

    getting user data , updating user data, and disabled user

--- Internet_Banking_Application/src/main/java/com/iba/main/userservice/service/UserEntityService.java
+++ Internet_Banking_Application/src/main/java/com/iba/main/userservice/service/UserEntityService.java
@@ -3,7 +3,12 @@ package com.iba.main.userservice.service;
 import com.iba.main.userservice.model.UserEntity;
 
 public interface UserEntityService {
-	
+
 	void saveUserEntityData(UserEntity userEntity);
 
+	UserEntity getuserEntity(Long id);
+
+	UserEntity updateUserEntity(UserEntity userEntity);
+	
+	void deleteUserData(Long id);
 }
--- Internet_Banking_Application/src/main/java/com/iba/main/userservice/service/UserEntityServiceImpl.java
+++ Internet_Banking_Application/src/main/java/com/iba/main/userservice/service/UserEntityServiceImpl.java
@@ -9,18 +9,39 @@ import com.iba.main.userservice.repo.UserEntityRepository;
 
 @Service
 public class UserEntityServiceImpl implements UserEntityService {
-	
+
 	@Autowired
 	private UserEntityRepository userEntityRepository;
 
 	@Override
 	public void saveUserEntityData(UserEntity userEntity) {
 		// TODO Auto-generated method stub
-		
 		userEntity.setStatus(Status.CREATED);
 		userEntityRepository.save(userEntity);
-		
+
 	}
-	
+
+	@Override
+	public UserEntity getuserEntity(Long id) {
+		// TODO Auto-generated method stub
+		UserEntity userEntity = userEntityRepository.findById(id).get();
+		return userEntity;
+	}
+
+	@Override
+	public UserEntity updateUserEntity(UserEntity userEntity) {
+		// TODO Auto-generated method stub
+		UserEntity userEntity2 = userEntityRepository.save(userEntity);
+		return userEntity2;
+	}
+
+	@Override
+	public void deleteUserData(Long id) {
+		// TODO Auto-generated method stub
+		UserEntity userEntity = userEntityRepository.findById(id).get();
+		userEntity.setStatus(Status.DISABLED);
+		userEntityRepository.save(userEntity);
+
+	}
 
 }
